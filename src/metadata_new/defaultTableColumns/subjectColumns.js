3a811d39d97277979dfd32e8133c208189370c7b
archgogo <archana.golla@medisin.uio.no>
2023-07-07 17:59:21 +0200

    removed dataType property
    Removed datatype property and modified type property to 'input' or 'inputName'

--- src/metadata_new/defaultTableColumns/subjectColumns.js
+++ src/metadata_new/defaultTableColumns/subjectColumns.js
@@ -5,20 +5,20 @@ var controlledTermsVersion = "v1";
 
 // New def:
 let columnProps = [
-    { key: 'SubjectID',     type: 'input', dataType: 'string', isMandatory: 'yes'}, 
-    { key: 'Age',           type: 'input', dataType: 'numerical', isMandatory: 'no'},
-    { key: 'AgeUnit',       type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/unitOfMeasurement`, filter: 'AgeUnit', dataType: 'string', isMandatory: 'no'},
-    { key: 'Weight',        type: 'input', dataType: 'numerical', isMandatory: 'no'}, 
-    { key: 'WeightUnit',    type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/unitOfMeasurement`, filter: 'WeightUnit', dataType: 'string', isMandatory: 'no'}, 
-    { key: 'AgeCategory',   type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/ageCategory`, dataType: 'string', isMandatory: 'yes'},
-    { key: 'BiologicalSex', type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/biologicalSex`, dataType: 'string', isMandatory: 'no'},
-    { key: 'Pathology',     type: 'dropdown', options: [`controlledTerms/${controlledTermsVersion}/disease`, `controlledTerms/${controlledTermsVersion}/diseaseModel`], dataType: 'string', isMandatory: 'no'},
-    { key: 'Handedness',    type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/handedness`, dataType: 'string', isMandatory: 'no'},
-    { key: 'Species',       type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/species`, dataType: 'string', isMandatory: 'yes'},
-    { key: 'Strain',        type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/strain`, dataType: 'string', isMandatory: 'no'}, // Todo: dependent on species selection
-    { key: 'SubjectAttribute',  type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/subjectAttribute`, dataType: 'string', isMandatory: 'no'},
-    { key: 'AdditionalRemarks', type: 'input', dataType: 'string', isMandatory: 'no'},
-    { key: 'IsPartOf',          type: 'dropdown', dataType: 'string', isMandatory: 'no'}
+    { key: 'SubjectID',     type: 'input', isMandatory: 'yes'}, 
+    { key: 'Age',           type: 'inputNumber', isMandatory: 'no'},
+    { key: 'AgeUnit',       type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/unitOfMeasurement`, filter: 'AgeUnit', isMandatory: 'no'},
+    { key: 'Weight',        type: 'inputNumber', isMandatory: 'no'}, 
+    { key: 'WeightUnit',    type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/unitOfMeasurement`, filter: 'WeightUnit', isMandatory: 'no'}, 
+    { key: 'AgeCategory',   type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/ageCategory`, isMandatory: 'yes'},
+    { key: 'BiologicalSex', type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/biologicalSex`, isMandatory: 'no'},
+    { key: 'Pathology',     type: 'dropdown', options: [`controlledTerms/${controlledTermsVersion}/disease`, `controlledTerms/${controlledTermsVersion}/diseaseModel`], isMandatory: 'no'},
+    { key: 'Handedness',    type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/handedness`, isMandatory: 'no'},
+    { key: 'Species',       type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/species`, isMandatory: 'yes'},
+    { key: 'Strain',        type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/strain`, isMandatory: 'no'}, // Todo: dependent on species selection
+    { key: 'SubjectAttribute',  type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/subjectAttribute`, isMandatory: 'no'},
+    { key: 'AdditionalRemarks', type: 'input', isMandatory: 'no'},
+    { key: 'IsPartOf',          type: 'dropdown', isMandatory: 'no'}
 ]
 
 //export default columnProps
--- src/metadata_new/defaultTableColumns/tissueSampleColumns.js
+++ src/metadata_new/defaultTableColumns/tissueSampleColumns.js
@@ -3,24 +3,24 @@ var controlledTermsVersion = "v1";
 
 // New def:
 let columnProps = [
-    { key: 'TissueSampleID',            type: 'input', dataType: 'string', isMandatory: 'yes'}, 
-    { key: 'Origin',                    type: 'dropdown', options: [`controlledTerms/${controlledTermsVersion}/cellType`, `controlledTerms/${controlledTermsVersion}/organ`], dataType: 'string', isMandatory: 'yes'}, 
+    { key: 'TissueSampleID',            type: 'input', isMandatory: 'yes'}, 
+    { key: 'Origin',                    type: 'dropdown', options: [`controlledTerms/${controlledTermsVersion}/cellType`, `controlledTerms/${controlledTermsVersion}/organ`], isMandatory: 'yes'}, 
     //{ key: 'AnatomicalLocation',        type: 'input', options: [`controlledTerms/${controlledTermsVersion}/UBERONParcellation`, `SANDS/${sandsVersion}/atlas/parcellationEntity`, `SANDS/${sandsVersion}/atlas/parcellationEntityVersion`]},
-    { key: 'AnatomicalLocation',        type: 'dropdown',  options: `controlledTerms/${controlledTermsVersion}/UBERONParcellation`, dataType: 'string', isMandatory: 'no'}, // Todo: add organ to this list
-    { key: 'Age',                       type: 'input', dataType: 'numerical', isMandatory: 'no'},
-    { key: 'AgeUnit',                   type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/unitOfMeasurement`, filter: 'AgeUnit', dataType: 'string', isMandatory: 'no'},
-    { key: 'Weight',                    type: 'input', dataType: 'numerical', isMandatory: 'no'}, 
-    { key: 'WeightUnit',                type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/unitOfMeasurement`, filter: 'WeightUnit', dataType: 'string', isMandatory: 'no'},
-    { key: 'BiologicalSex',             type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/biologicalSex`, dataType: 'string', isMandatory: 'no'},
-    { key: 'Pathology',                 type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/disease`, dataType: 'string', isMandatory: 'no'},
-    { key: 'Laterality',                type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/laterality`, dataType: 'string', isMandatory: 'no'},
-    { key: 'Species',                   type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/species`, dataType: 'string', isMandatory: 'yes'},
-    { key: 'Strain',                    type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/strain`, dataType: 'string', isMandatory: 'no'}, // Todo: dependent on species selection
-    { key: 'TissueSampleAttribute',     type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/tissueSampleAttribute`, dataType: 'string', isMandatory: 'no'},
-    { key: 'TissueSampleType',          type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/tissueSampleType`, dataType: 'string', isMandatory: 'yes'},
-    { key: 'AdditionalRemarks',         type: 'input', dataType: 'string', isMandatory: 'no'},
-    { key: 'IsPartOf',                  type: 'dropdown', dataType: 'string', isMandatory: 'no'},
-    { key: 'DescendedFromSubjectID',    type: 'dropdown', dataType: 'string', isMandatory: 'no'}
+    { key: 'AnatomicalLocation',        type: 'dropdown',  options: `controlledTerms/${controlledTermsVersion}/UBERONParcellation`, isMandatory: 'no'}, // Todo: add organ to this list
+    { key: 'Age',                       type: 'inputNumber', isMandatory: 'no'},
+    { key: 'AgeUnit',                   type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/unitOfMeasurement`, filter: 'AgeUnit', isMandatory: 'no'},
+    { key: 'Weight',                    type: 'inputNumber', isMandatory: 'no'}, 
+    { key: 'WeightUnit',                type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/unitOfMeasurement`, filter: 'WeightUnit', isMandatory: 'no'},
+    { key: 'BiologicalSex',             type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/biologicalSex`, isMandatory: 'no'},
+    { key: 'Pathology',                 type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/disease`, isMandatory: 'no'},
+    { key: 'Laterality',                type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/laterality`, isMandatory: 'no'},
+    { key: 'Species',                   type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/species`, isMandatory: 'yes'},
+    { key: 'Strain',                    type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/strain`, isMandatory: 'no'}, // Todo: dependent on species selection
+    { key: 'TissueSampleAttribute',     type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/tissueSampleAttribute`, isMandatory: 'no'},
+    { key: 'TissueSampleType',          type: 'dropdown', options: `controlledTerms/${controlledTermsVersion}/tissueSampleType`, isMandatory: 'yes'},
+    { key: 'AdditionalRemarks',         type: 'input', isMandatory: 'no'},
+    { key: 'IsPartOf',                  type: 'dropdown', isMandatory: 'no'},
+    { key: 'DescendedFromSubjectID',    type: 'dropdown', isMandatory: 'no'}
 ]
 
 //export default columnProps
