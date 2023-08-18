2906b41c3ccf665f8d851abbb151ba7c31f3d3df
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2023-08-18 13:16:36 +0200

    forgot to close bracket

--- src/components/AppsAndAnalysis.js
+++ src/components/AppsAndAnalysis.js
@@ -29,7 +29,7 @@ function AppsAndAnalysisPage(props) {
 
 
     // disable the "Created Brains" menu if WebIlastik or NutilWeb is selected
-    if (appUrl.includes("https://app.ilastik.org/") {
+    if (appUrl.includes("https://app.ilastik.org/")) {
           iframe.setAttribute("sandbox", "allow-scripts allow-same-origin");
     }
     if (appUrl.includes("https://app.ilastik.org/") || appUrl.includes("https://127.0.0.1:3000/")) {
