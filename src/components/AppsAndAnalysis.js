125eed81c1a016019e222812cebde9f38b01d44f
Harry Carey <harry.carey95@gmail.com>
2023-10-16 17:25:57 +0200

    progress widget is now functional

--- src/components/AppsAndAnalysis.js
+++ src/components/AppsAndAnalysis.js
@@ -1,4 +1,4 @@
-import { Menu, Button, Tooltip, Modal , Table} from 'antd';
+import { Menu, Button, Tooltip, Modal, Table } from 'antd';
 import React, { useState, useEffect } from 'react';
 import { InfoCircleOutlined } from '@ant-design/icons';
 
@@ -16,9 +16,9 @@ function AppsAndAnalysisPage(props) {
     // setCurrentlySelectedBrain(""); // reset currentlySelectedBrain to empty string
     // set classname of all menu items to "ant-menu-item ant-menu-item-only-child"
     // setMenuItems(menuItems.map(item => ({ ...item, props: { ...item.props} })));
-    
+
     const iframe = document.getElementById("apps-iframe");
-    
+
     // if iframe exists change the style
     // if (iframe) {
     //     // hack bc ilastik resizes the iframe 
@@ -48,7 +48,7 @@ function AppsAndAnalysisPage(props) {
   const launchProgressWidget = () => {
     setProgressWidgetOpen(true);
   }
-  
+
 
   const handleFullScreen = () => {
     const iframe = document.getElementById("apps-iframe");
@@ -69,7 +69,7 @@ function AppsAndAnalysisPage(props) {
   };
 
   let menuStyle = {
-    backgroundColor:'#fbf8fc',
+    backgroundColor: '#fbf8fc',
   };
 
   const [menuItems, setMenuItems] = useState([]);
@@ -85,44 +85,59 @@ function AppsAndAnalysisPage(props) {
           const name = object.name.substring(object.name.lastIndexOf("/") + 1);
           const nameWithoutExtension = name.split(".").slice(0, -1).join(".");
           // request each json and check if it has a "markers" field
-          fetch(`https://data-proxy.ebrains.eu/api/v1/buckets/${props.keyValue}/${object.name}?inline=false&redirect=false`)
+          const cacheBuster = new Date().getTime(); // generate a unique timestamp
+          fetch(`https://data-proxy.ebrains.eu/api/v1/buckets/${props.keyValue}/${object.name}?inline=false&redirect=false&_=${cacheBuster}`)
             .then(response => response.json())
             .then(data => data.url)
-            .then(req_url => {
-              console.log("req_url", req_url)
-              fetch(req_url)
-                .then(resp => {
-                  console.log("resp", resp.json())
-                  // if it has a markers field, then it is a completed brain
-                  let WebWarp;
-                  if (data.markers) {
-                    WebWarp = true;
+            .then(req_url => fetch(req_url))
+            .then(resp => resp.json())
+            .then(resp => {
+              var WebAlign = '✔️';
+              var WebWarp = '❌';
+              for (let i = 0; i < resp["sections"].length; i++) {
+                if (resp["sections"][i].markers) {
+                  WebWarp = '✔️';
+                  break;
+                }
+              }
+              // update onGoingBrains
+
+              fetch(`https://data-proxy.ebrains.eu/api/v1/buckets/${props.keyValue}?prefix=.nesysWorkflowFiles/ilastikOutputs/${nameWithoutExtension}&delimiter=%2F&limit=50`)
+                .then(response => response.json())
+                .then(data => {
+                  var WebIlastik = '❌';
+                  console.log("ilastikdata", data)
+                  if (data.objects.length > 1) {
+                    WebIlastik = '✔️';
                   }
-                  else {
-                    WebWarp = false;
-                  }
-                  // update onGoingBrains
-                  let temp_data = {
-                    "key": index,
-                    "BrainID": nameWithoutExtension,
-                    "WebAlign": true,
-                    "WebWarp": WebWarp,
-                    "WebIlastik": false,
-                    "NutilWeb": false
-                  };
-                  onGoingBrainsTemp.push(temp_data);
-                  index++;
-                  if (index === items.length) {
-                    setOnGoingBrains(onGoingBrainsTemp);
-                    console.log("onGoingBrains", onGoingBrains)
-                  }
-                });
-            });
+                  fetch(`https://data-proxy.ebrains.eu/api/v1/buckets/${props.keyValue}?prefix=.nesysWorkflowFiles/pointClouds/${nameWithoutExtension}&delimiter=%2F&limit=50`)
+                    .then(response => response.json())
+                    .then(data => {
+                      let NutilWeb = '❌';
+                      if (data.objects.length > 1) {
+                        NutilWeb = '✔️';
+                      }
+                      let temp_data = {
+                        "key": index,
+                        "BrainID": nameWithoutExtension,
+                        "WebAlign": '✔️',
+                        "WebWarp": WebWarp,
+                        "WebIlastik": WebIlastik,
+                        "NutilWeb": NutilWeb
+                      };
+                      onGoingBrainsTemp.push(temp_data);
+                      index++;
+                      if (index === items.length) {
+                        setOnGoingBrains(onGoingBrainsTemp);
+                      }
+                      // handle temp_data
+                    })
+                })
+            })
         });
       });
   };
 
-  
   useEffect(() => {
     updateOnGoingBrains();
   }, []);
@@ -142,101 +157,101 @@ function AppsAndAnalysisPage(props) {
   console.log("unsetSelectedApp", unsetSelectedApp)
   console.log("unsetSelectedApp", unsetSelectedApp === "")
   return (
-    <div style={{ display: "flex", flexDirection: "row"}}>
-        <div id="sideBar" style={{ ...menuStyle ,display: "flex", flexDirection: "row"}}>
-      <div id="sideBarApps" style={{ ...menuStyle, width: "10vw"}}>
-        <div style={{ fontWeight: 'bold', marginTop:'3rem', marginBottom:'1rem', fontSize: '1.5em'}}>Apps</div>
-        <Menu style={{ justifyContent: "center" , backgroundColor:'#fbf8fc'}}>
-          <Menu.Item onClick={() => handleAppSelect(`https://webalign.apps-dev.hbp.eu/?clb-collab-id=${props.keyValue}`)}>
-            <span style={{ marginLeft: ".3125rem", marginRight: ".3125rem" }}>WebAlign</span>
-            <Tooltip title="Launch WebAlign">
-              <Button shape="circle" size="small" style={{ marginLeft: "1.1rem", marginRight: ".3125rem" }}>
-                <InfoCircleOutlined />
-              </Button>
-            </Tooltip>
-          </Menu.Item>
-          <Menu.Item onClick={() => handleAppSelect(`https://lz-nl.apps.hbp.eu/webwarp.php?clb-collab-id=${props.keyValue}`)}>
-            <span style={{ marginLeft: ".3125rem", marginRight: ".3125rem" }}>WebWarp</span>
-            <Tooltip title="Launch WebWarp">
-              <Button shape="circle" size="small" style={{ marginLeft: "1rem", marginRight: ".3125rem" }}>
-                <InfoCircleOutlined />
-              </Button>
-            </Tooltip>
-          </Menu.Item>
-          <Menu.Item onClick={() => handleAppSelect(`https://app.ilastik.org/public/nehuba/index.html?output_path_pattern=/.nesysWorkflowFiles/ilastikOutputs/{parent_dir_name}/{name}.{extension}&ebrains_bucket_name=${props.keyValue}&ebrains_bucket_path=.nesysWorkflowFiles/zippedPyramids/#!{"layout":"xy"}`)}>
-            <span style={{ marginLeft: ".3125rem", marginRight: ".3125rem" }}>WebIlastik</span>
-            <Tooltip title="Launch WebIlastik">
-              <Button shape="circle" size="small" style={{ marginLeft: "0.8rem", marginRight: ".3125rem" }} >
-                <InfoCircleOutlined />
-              </Button>
-            </Tooltip>
-          </Menu.Item>
-          <Menu.Item onClick={() => handleAppSelect(`https://pynutil-test.apps.hbp.eu/?clb-collab-id=${props.keyValue}`)} style={{ display: "flex", alignItems: "center" }}>
-            <span style={{ marginLeft: ".3125rem", marginRight: ".3125rem" }}>NutilWeb</span>
-            <Tooltip title="Launch NutilWeb">
-              <Button shape="circle" size="small" style={{ marginLeft: "1rem", marginRight: ".3125rem" }}>
-                <InfoCircleOutlined />
-              </Button>
-            </Tooltip>
-          </Menu.Item>
-        </Menu>
-    <Button type='primary' onClick={handleFullScreen} style={{ width: "8rem", height:"4rem", whiteSpace: "normal", marginTop:'1rem' }} disabled={!currentlySelectedBrain}>Full Screen the Application</Button> 
-    <br></br>
-    <Button type='primary' onClick={launchProgressWidget} style={{ width: "8rem", height:"4rem", whiteSpace: "normal", marginTop:'1rem' }} >View Progress Widget</Button> 
-    <Modal title="Progress Widget" visible={progressWidgetOpen} onOk={() => setProgressWidgetOpen(false)} onCancel={() => setProgressWidgetOpen(false)}>
-    <Table columns={[
-  {
-    title: 'Brain ID',
-    dataIndex: 'BrainID',
-  },
-  {
-    title: 'WebAlign',
-    dataIndex: 'WebAlign',
-  },
-  {
-    title: 'WebWarp',
-    dataIndex: 'WebWarp',
-  },
-  {
-    title: 'WebIlastik',
-    dataIndex: 'WebIlastik',
-  },
-  {
-    title: 'NutilWeb',
-    dataIndex: 'NutilWeb',
-  },
-]} dataSource={onGoingBrains} size="small" />
-    </Modal>
-    </div>
-      <div id="sideBarBrains" style={{ ...menuStyle, 'white-space': 'nowrap',  maxWidth: disableCreatedBrains ? 0 : '10vw', width:'10vw', overflow: 'hidden', transition: 'max-width 0.5s ease-in-out' }}>
-        <div style={{ fontWeight: 'bold' ,marginTop:'3rem', fontSize: '1.5em',marginBottom:'1rem'}}>{BrainMenuHeader}</div>
-        <Menu disabled={disableCreatedBrains} onSelect={handleMenuSelect} selectedKeys={selectedKeys} style={{backgroundColor:'#fbf8fc'}}>
-          {menuItems}
-        </Menu>
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div id="sideBar" style={{ ...menuStyle, display: "flex", flexDirection: "row" }}>
+        <div id="sideBarApps" style={{ ...menuStyle, width: "10vw" }}>
+          <div style={{ fontWeight: 'bold', marginTop: '3rem', marginBottom: '1rem', fontSize: '1.5em' }}>Apps</div>
+          <Menu style={{ justifyContent: "center", backgroundColor: '#fbf8fc' }}>
+            <Menu.Item onClick={() => handleAppSelect(`https://webalign.apps-dev.hbp.eu/?clb-collab-id=${props.keyValue}`)}>
+              <span style={{ marginLeft: ".3125rem", marginRight: ".3125rem" }}>WebAlign</span>
+              <Tooltip title="Launch WebAlign">
+                <Button shape="circle" size="small" style={{ marginLeft: "1.1rem", marginRight: ".3125rem" }}>
+                  <InfoCircleOutlined />
+                </Button>
+              </Tooltip>
+            </Menu.Item>
+            <Menu.Item onClick={() => handleAppSelect(`https://lz-nl.apps.hbp.eu/webwarp.php?clb-collab-id=${props.keyValue}`)}>
+              <span style={{ marginLeft: ".3125rem", marginRight: ".3125rem" }}>WebWarp</span>
+              <Tooltip title="Launch WebWarp">
+                <Button shape="circle" size="small" style={{ marginLeft: "1rem", marginRight: ".3125rem" }}>
+                  <InfoCircleOutlined />
+                </Button>
+              </Tooltip>
+            </Menu.Item>
+            <Menu.Item onClick={() => handleAppSelect(`https://app.ilastik.org/public/nehuba/index.html?output_path_pattern=/.nesysWorkflowFiles/ilastikOutputs/{parent_dir_name}/{name}.{extension}&ebrains_bucket_name=${props.keyValue}&ebrains_bucket_path=.nesysWorkflowFiles/zippedPyramids/#!{"layout":"xy"}`)}>
+              <span style={{ marginLeft: ".3125rem", marginRight: ".3125rem" }}>WebIlastik</span>
+              <Tooltip title="Launch WebIlastik">
+                <Button shape="circle" size="small" style={{ marginLeft: "0.8rem", marginRight: ".3125rem" }} >
+                  <InfoCircleOutlined />
+                </Button>
+              </Tooltip>
+            </Menu.Item>
+            <Menu.Item onClick={() => handleAppSelect(`https://pynutil-test.apps.hbp.eu/?clb-collab-id=${props.keyValue}`)} style={{ display: "flex", alignItems: "center" }}>
+              <span style={{ marginLeft: ".3125rem", marginRight: ".3125rem" }}>NutilWeb</span>
+              <Tooltip title="Launch NutilWeb">
+                <Button shape="circle" size="small" style={{ marginLeft: "1rem", marginRight: ".3125rem" }}>
+                  <InfoCircleOutlined />
+                </Button>
+              </Tooltip>
+            </Menu.Item>
+          </Menu>
+          <Button type='primary' onClick={handleFullScreen} style={{ width: "8rem", height: "4rem", whiteSpace: "normal", marginTop: '1rem' }} disabled={!currentlySelectedBrain}>Full Screen the Application</Button>
+          <br></br>
+          <Button type='primary' onClick={launchProgressWidget} style={{ width: "8rem", height: "4rem", whiteSpace: "normal", marginTop: '1rem' }} >View Progress Widget</Button>
+          <Modal title="Progress Widget" visible={progressWidgetOpen} onOk={() => setProgressWidgetOpen(false)} onCancel={() => setProgressWidgetOpen(false)}>
+            <Table columns={[
+              {
+                title: 'Brain ID',
+                dataIndex: 'BrainID',
+              },
+              {
+                title: 'WebAlign',
+                dataIndex: 'WebAlign',
+              },
+              {
+                title: 'WebWarp',
+                dataIndex: 'WebWarp',
+              },
+              {
+                title: 'WebIlastik',
+                dataIndex: 'WebIlastik',
+              },
+              {
+                title: 'NutilWeb',
+                dataIndex: 'NutilWeb',
+              },
+            ]} dataSource={onGoingBrains} size="small" />
+          </Modal>
+        </div>
+        <div id="sideBarBrains" style={{ ...menuStyle, 'white-space': 'nowrap', maxWidth: disableCreatedBrains ? 0 : '10vw', width: '10vw', overflow: 'hidden', transition: 'max-width 0.5s ease-in-out' }}>
+          <div style={{ fontWeight: 'bold', marginTop: '3rem', fontSize: '1.5em', marginBottom: '1rem' }}>{BrainMenuHeader}</div>
+          <Menu disabled={disableCreatedBrains} onSelect={handleMenuSelect} selectedKeys={selectedKeys} style={{ backgroundColor: '#fbf8fc' }}>
+            {menuItems}
+          </Menu>
+        </div>
       </div>
-    </div>
-    <div id="iframeContainer" style={{ width: '90vw', height: '100vh', float: 'right' }}>
-      {(disableCreatedBrains & (selectedApp!=="")) ? (
-  <iframe id="apps-iframe" title="Apps and Analysis" src={selectedApp} style={{ background: 'white', height: 'calc(100vh - 3.875rem)', width: '100%', border: '0' , overflow:'auto'}}></iframe>
-) :(menuItems.length == 0) ?(
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background:"white"}}>
-  <span style={{ fontSize: '2em', fontWeight: 'bold', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background: 'white' }}>  You havent created any brains,<br></br> start by creating a brain in the file creator tab
-  </span>    
-</div>
-)
+      <div id="iframeContainer" style={{ width: '90vw', height: '100vh', float: 'right' }}>
+        {(disableCreatedBrains & (selectedApp !== "")) ? (
+          <iframe id="apps-iframe" title="Apps and Analysis" src={selectedApp} style={{ background: 'white', height: 'calc(100vh - 3.875rem)', width: '100%', border: '0', overflow: 'auto' }}></iframe>
+        ) : (menuItems.length == 0) ? (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background: "white" }}>
+            <span style={{ fontSize: '2em', fontWeight: 'bold', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background: 'white' }}>  You havent created any brains,<br></br> start by creating a brain in the file creator tab
+            </span>
+          </div>
+        )
 
-  : !unsetSelectedApp ? (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background:"white"}}>
-    <span style={{ fontSize: '2em', fontWeight: 'bold', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background: 'white' }}>Please start by selecting an app</span>    
-  </div>
-) : currentlySelectedBrain === "" ? (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background:"white"}}>
-    <span style={{ fontSize: '2em', fontWeight: 'bold', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background: 'white' }}>Please select a brain to view in the app.</span>    
-  </div>
-) : (
-  <iframe id="apps-iframe" title="Apps and Analysis" src={selectedApp} style={{ background: 'white', height: 'calc(100vh - 3.875rem)', width: '100%', border: '0' }} ></iframe>
-)}
-    </div>
+          : !unsetSelectedApp ? (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background: "white" }}>
+              <span style={{ fontSize: '2em', fontWeight: 'bold', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background: 'white' }}>Please start by selecting an app</span>
+            </div>
+          ) : currentlySelectedBrain === "" ? (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background: "white" }}>
+              <span style={{ fontSize: '2em', fontWeight: 'bold', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', background: 'white' }}>Please select a brain to view in the app.</span>
+            </div>
+          ) : (
+            <iframe id="apps-iframe" title="Apps and Analysis" src={selectedApp} style={{ background: 'white', height: 'calc(100vh - 3.875rem)', width: '100%', border: '0' }} ></iframe>
+          )}
+      </div>
     </div>
   )
 }
